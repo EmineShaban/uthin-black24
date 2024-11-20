894b64a308e1223b6fa2ab7708285dca74cf6f3c
Emine Shaban <emineshaban@icloud.com>
2024-11-20 15:53:10 +0200

    .

---
+++ index.php
@@ -0,0 +1,319 @@
+// black friday INDEX
+//ENG
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "with code ";
+$LANG['shop_sale'] = "shop sale";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>eliminates </b> bloating</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>reduces </b> water retention</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>activates </b> slimming</li>
+HTML;
+$LANG['hurry_up'] = "<b>hurry up! </b> now is the time!";
+$LANG['up_to_65'] = <<<HTML
+<p>up to</p>
+<p>65</p>
+<p><span>%</span> <span>off</span></p>
+HTML;
+
+
+
+//DE
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "mit dem Code ";
+$LANG['shop_sale'] = "Verkauf im Shop";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Beseitigt</b> Blähungen</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Reduziert</b> Wassereinlagerungen</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Aktiviert</b> Schlankheit</li>
+HTML;
+$LANG['hurry_up'] = "<b>BEEILUNG! </b> Jetzt ist die Zeit!";
+$LANG['up_to_65'] = <<<HTML
+<p>Bis zu</p>
+<p>65</p>
+<p><span>%</span> <span>Rabatt</span></p>
+HTML;
+
+
+//NL
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "met code ";
+$LANG['shop_sale'] = "WINKEL UITVERKOOP";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Elimineert</b> een opgeblazen gevoel</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Vermindert</b> vochtretentie</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Activeert</b> afslanken</li>
+HTML;
+$LANG['hurry_up'] = "<b>HAAST JE! </b> Nu is het moment!";
+$LANG['up_to_65'] = <<<HTML
+<p>Tot</p>
+<p>65</p>
+<p><span>%</span> <span>korting</span></p>
+HTML;
+
+
+//CZ
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "s kódem ";
+$LANG['shop_sale'] = "VÝPRODEJ";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Odstraňuje</b> nadýmání</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Snižuje</b> zadržování vody</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Aktivuje</b> hubnutí</li>
+HTML;
+$LANG['hurry_up'] = "<b>PONÁHLEJTE SE! </b> Teď je ten čas!";
+$LANG['up_to_65'] = <<<HTML
+<p>Až</p>
+<p>65</p>
+<p><span>%</span> <span>sleva</span></p>
+HTML;
+
+
+
+//FR
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "avec le code ";
+$LANG['shop_sale'] = "Vente en magasin";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Élimine</b> les ballonnements</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Réduit</b> la rétention d'eau</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Active</b> l'amincissement</li>
+HTML;
+$LANG['hurry_up'] = "<b>Dépêche-toi! </b> C'est le moment !";
+$LANG['up_to_65'] = <<<HTML
+<p>Jusqu'à</p>
+<p>65</p>
+<p><span>%</span> <span>de réduction</span></p>
+HTML;
+
+
+
+//HR
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "sa šifrom ";
+$LANG['shop_sale'] = " AKCIJA U TRGOVINI";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Uklanja</b> nadutost</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Smanjuje</b> zadržavanje vode</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Aktivira</b> mršavljenje
+</li>
+HTML;
+$LANG['hurry_up'] = "<b>Požuri! </b> Sada je vrijeme!";
+$LANG['up_to_65'] = <<<HTML
+<p>Do</p>
+<p>65</p>
+<p><span>%</span> <span>popusta</span></p>
+HTML;
+
+
+
+
+//HU
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "kóddal ";
+$LANG['shop_sale'] = "SHOP AKCIÓ";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Megszünteti</b> a puffadást</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Csökkenti</b> a vízvisszatartást</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Aktiválja</b> a fogyást</li>
+HTML;
+$LANG['hurry_up'] = "<b>SIESSEN </b> Most van itt az idő!";
+$LANG['up_to_65'] = <<<HTML
+<p>Akár</p>
+<p>65</p>
+<p><span>%</span> <span>kedvezmény</span></p>
+HTML;
+
+
+//SI
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "s kodo ";
+$LANG['shop_sale'] = " AKCIJA V TRGOVINI";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Odpravlja</b> napihnjenost</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Zmanjšuje</b> zadrževanje vode</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Aktivira</b> hujšanje</li>
+HTML;
+$LANG['hurry_up'] = "<b>Pohiti! </b> Zdaj je čas!";
+$LANG['up_to_65'] = <<<HTML
+<p>Do</p>
+<p>65</p>
+<p><span>%</span> <span>popusta</span></p>
+HTML;
+
+
+
+//SK
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "s kódom ";
+$LANG['shop_sale'] = "VÝPREDAJ";
+$LANG['bullets_black24'] = <<<HTML
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Odstraňuje</b> nadúvanie</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Znižuje</b> zadržiavanie vody</li>
+<li><img src="/cdn/uthin/inc/assets-general/black24/Index Banner_Uthin-06.webp" alt=""> <b>Aktivuje</b> chudnutie</li>
+HTML;
+$LANG['hurry_up'] = "<b>PONÁHĽAJTE SA </b> Teraz je ten čas!";
+$LANG['up_to_65'] = <<<HTML
+<p>Až</p>
+<p>65</p>
+<p><span>%</span> <span>zľava</span></p>
+HTML;
+
+
+
+
+
+
+
+
+
+
+
+//=======================================================================================
+//=======================================================================================
+//=======================================================================================
+//=======================================================================================
+//=======================================================================================
+
+//STEP2
+//ENG
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "with code ";
+$LANG['hurry_up'] = "<b>hurry up! </b> now is the time!";
+$LANG['up_to_65'] = <<<HTML
+<p>up to</p>
+<p>65</p>
+<p><span>%</span> <span>off</span></p>
+HTML;
+
+
+ 
+
+//DE
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "mit dem Code ";
+$LANG['hurry_up'] = "<b>BEEILUNG! </b> Jetzt ist die Zeit!";
+$LANG['up_to_65'] = <<<HTML
+<p>Bis zu</p>
+<p>65</p>
+<p><span>%</span> <span>Rabatt</span></p>
+HTML;
+
+
+//NL
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "met code ";
+$LANG['hurry_up'] = "<b>HAAST JE! </b> Nu is het moment!";
+$LANG['up_to_65'] = <<<HTML
+<p>Tot</p>
+<p>65</p>
+<p><span>%</span> <span>korting</span></p>
+HTML;
+
+
+//CZ
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "s kódem ";
+$LANG['hurry_up'] = "<b>PONÁHLEJTE SE! </b> Teď je ten čas!";
+$LANG['up_to_65'] = <<<HTML
+<p>Až</p>
+<p>65</p>
+<p><span>%</span> <span>sleva</span></p>
+HTML;
+
+
+
+//FR
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "avec le code ";
+$LANG['hurry_up'] = "<b>Dépêche-toi! </b> C'est le moment !";
+$LANG['up_to_65'] = <<<HTML
+<p>Jusqu'à</p>
+<p>65</p>
+<p><span>%</span> <span>de réduction</span></p>
+HTML;
+
+
+
+//HR
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "sa šifrom ";
+$LANG['hurry_up'] = "<b>Požuri! </b> Sada je vrijeme!";
+$LANG['up_to_65'] = <<<HTML
+<p>Do</p>
+<p>65</p>
+<p><span>%</span> <span>popusta</span></p>
+HTML;
+
+
+
+
+//HU
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "kóddal ";
+$LANG['hurry_up'] = "<b>SIESSEN </b> Most van itt az idő!";
+$LANG['up_to_65'] = <<<HTML
+<p>Akár</p>
+<p>65</p>
+<p><span>%</span> <span>kedvezmény</span></p>
+HTML;
+
+
+//SI
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "s kodo ";
+$LANG['hurry_up'] = "<b>Pohiti! </b> Zdaj je čas!";
+$LANG['up_to_65'] = <<<HTML
+<p>Do</p>
+<p>65</p>
+<p><span>%</span> <span>popusta</span></p>
+HTML;
+
+
+
+//SK
+$LANG['black_friday_sale'] = <<<HTML
+black friday <span class="sale">sale</span>
+HTML;
+$LANG['with_code'] = "s kódom ";
+$LANG['hurry_up'] = "<b>PONÁHĽAJTE SA </b> Teraz je ten čas!";
+$LANG['up_to_65'] = <<<HTML
+<p>Až</p>
+<p>65</p>
+<p><span>%</span> <span>zľava</span></p>
+HTML;
+
+
